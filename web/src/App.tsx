54a2154bb37033acc88a1a7d7caa0ceef764a029
ormesino <pedromateus18@hotmail.com>
2023-02-01 18:29:15 -0300

    front: creating some components, styles and configuring tailwind

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,16 @@
 import './styles/global.css'
+import { Header } from './components/Header'
+import { Dashboard } from './components/Dashboard'
 
-import Habit from "./components/Habit"
+// import Habit from "./components/Habit"
 
-function App() {
+export function App() {
   return (
-    <div>
-      <Habit completed={3}/>
-      <Habit completed={0}/>
-      <Habit completed={2}/>
-      <Habit completed={1}/>
+    <div className='w-screen h-screen flex flex-col justify-center items-center'>
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+        <Header />
+        <Dashboard />
+      </div>
     </div>
   )
 }
-
-export default App
